eede497431e0aeec6b4b3afa0290266f9f1e2025
esmeraldo17 <esmeraldoalberto55@gmail.com>
2023-02-14 17:29:09 +0100

    feat: criando model da tabela users

---
+++ src/models/User.js
@@ -0,0 +1,28 @@
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      displayName: {
+        type: DataTypes.STRING,
+      },
+      email: {
+        type: DataTypes.STRING,
+        unique: true
+      },
+      password: {
+        type: DataTypes.STRING,
+      },
+      image: {
+        type: DataTypes.STRING
+      }
+    }, {
+      underscored: true,
+      tableName: 'users',
+      timestamps: false,
+    });
+  
+    return User;
+  }
